8d856f2a132bedeb9550e2c5fa0bde3c5be5484d
Wellington Menezes <ligton.ribeiro@gmail.com>
2025-09-04 15:16:26 -0300

    versão com alocação dinâmica

--- main.c
+++ main.c
@@ -2,12 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 #include <locale.h>
+#include <math.h>
 
-#define TAMANHO 200000
+#define TAMANHO 100000
 
 void selectionSort(int *V, int N);
 void bubbleSort(int *V, int N);
 void insertionSort(int *V, int N);
+
+void mergeSort(int *V, int inicio, int fim);
+void merge(int *V, int inicio, int meio, int fim);
+
 void printArray(int *V, int N);
 void gerarArrayDescrescente(int *V, int N);
 
@@ -15,17 +20,18 @@ int main(void)
 {
     setlocale(LC_ALL, "Portuguese");
 
-    int *arr1;
-    int *arr2;
-    int *arr3;
+    int *arr1 = (int *) malloc(TAMANHO * sizeof(int));
+    int *arr2 = (int *) malloc(TAMANHO * sizeof(int));
+    int *arr3 = (int *) malloc(TAMANHO * sizeof(int));
+    int *arr4 = (int *) malloc(TAMANHO * sizeof(int));
 
-    arr1 = (int *)malloc(TAMANHO * sizeof(int));
-    arr2 = (int *)malloc(TAMANHO * sizeof(int));
-    arr3 = (int *)malloc(TAMANHO * sizeof(int));
-
-    if (arr1 == NULL || arr2 == NULL || arr3 == NULL)
+    if (arr1 == NULL || arr2 == NULL || arr3 == NULL || arr4 == NULL)
     {
         printf("Erro de alocação de memória!\n");
+        if (arr1 != NULL) free(arr1);
+        if (arr2 != NULL) free(arr2);
+        if (arr3 != NULL) free(arr3);
+        if (arr4 != NULL) free(arr4);
         return 1;
     }
 
@@ -37,6 +43,7 @@ int main(void)
     {
         arr2[i] = arr1[i];
         arr3[i] = arr1[i];
+        arr4[i] = arr1[i];
     }
 
     // Ordenação por bolha
@@ -68,6 +75,13 @@ int main(void)
 
     free(arr3);
     arr3 = NULL;
+
+    //ordenação merge sort
+    inicio = clock();
+    mergeSort(arr4, 0, TAMANHO - 1);
+    fim = clock();
+    tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
+    printf("Tempo de execução do Merge Sort: %f segundos\n", tempo);
 
     return 0;
 }
@@ -125,6 +139,62 @@ void insertionSort(int *V, int N)
         }
         V[j] = atual;
     }
+}
+
+void mergeSort(int *V, int inicio, int fim)
+{
+    int meio;
+    if (inicio < fim)
+    {
+        meio = floor((inicio + fim)/2);
+        mergeSort(V, inicio, meio);
+        mergeSort(V, meio + 1, fim);
+        merge(V, inicio, meio, fim);
+    }
+}
+
+void merge(int *V, int inicio, int meio, int fim)
+{
+    int *temp, p1, p2, tamanho, i, j, k;
+    int fim1 = 0, fim2 = 0;
+    tamanho = fim - inicio + 1;
+    p1 = inicio;
+    p2 = meio + 1;
+
+    temp = (int *) malloc(tamanho * sizeof(int));
+
+    if (temp != NULL)
+    {
+        for (i = 0; i < tamanho; i++)
+        {
+            if (!fim1 && !fim2)
+            {
+                if (V[p1] < V[p2])
+                {
+                    temp[i] = V[p1++];
+                } else
+                {
+                    temp[i] = V[p2++];
+                }
+                if (p1>meio) fim1 = 1;
+                if (p2>fim) fim2 = 1;
+            } else
+            {
+                if (!fim1)
+                {
+                    temp[i] = V[p1++];
+                } else
+                {
+                    temp[i] = V[p2++];
+                }
+            }
+        }
+        for (j=0, k=inicio; j<tamanho; j++, k++)
+        {
+            V[k] = temp[j];
+        }
+    }
+    free(temp);
 }
 
 void gerarArrayDescrescente(int *V, int N)
